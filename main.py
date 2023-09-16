49f727ea7f7d1be869aa4bba5312285c317c9c53
hieuminh65 <hieuminhnguyen@usf.edu>
2023-09-16 15:40:16 -0400

    Add search to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from components.welcome import welcome
 from components.login import login
+from components.search import search
 
 def main():
     welcome()
     login()
+    search()
 
 
 if __name__ == "__main__":
