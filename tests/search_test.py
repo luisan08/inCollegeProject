da86c65355240e704c8c89d5a076107d47d8f998
NPatel-21 <142070922+NPatel-21@users.noreply.github.com>
2023-09-24 19:08:23 -0400

    Add more test cases for peopleSearch function()
    Changed the name of test file to search_test.py

---
+++ tests/search_test.py
@@ -0,0 +1,50 @@
+from components.search import jobSearch, peopleSearch, skillSearch
+import pytest
+
+@pytest.mark.parametrize("skillChoice",[1, 2, 3, 4, 5])
+def test_skill_search(skillChoice, monkeypatch, capfd):
+    monkeypatch.setattr('builtins.input', lambda _: skillChoice)
+    skillSearch()
+    out, _ = capfd.readouterr()
+    assert "Under construction." in out
+
+#Pytests for people function
+mock_accounts_data = """first,last
+John,Doe
+Alice,Smith
+"""
+mock_accounts = pd.read_csv(StringIO(mock_accounts_data))
+
+'''@patch('builtins.input', side_effect=['1', 'John', 'Doe', '2'])
+@patch('components.login.accounts', mock_accounts)
+def test_peopleSearch_existing_user(mock_input, capfd):
+    # Mock the 'input' function to provide user input
+    peopleSearch()
+    out, _ = capfd.readouterr()
+    
+    assert "Would you like to connect with John Doe?" in out'''
+
+@patch('builtins.input', side_effect=['1', 'Alice', 'Wonderland', '2'])
+@patch('components.login.accounts', mock_accounts)
+def test_peopleSearch_non_existing_user(mock_input, capfd):
+    # Mock the 'input' function to provide user input
+    peopleSearch()
+    out, _ = capfd.readouterr()
+    
+    assert "Username was not found." in out
+
+@patch('builtins.input', side_effect=['3', '2'])
+def test_peopleSearch_return_to_search(mock_input, capfd):
+    # Mock the 'input' function to provide user input
+    peopleSearch()
+    out, _ = capfd.readouterr()
+    
+    assert "Returning to People Search." in out
+
+@patch('builtins.input', side_effect=['4', '2'])
+def test_peopleSearch_invalid_option(mock_input, capfd):
+    # Mock the 'input' function to provide user input
+    peopleSearch()
+    out, _ = capfd.readouterr()
+    
+    assert "Invalid option. Returning to People Search." in out
