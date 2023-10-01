96ebd562a7eaa858f34ea9e1fc9de870ebf753c0
Hieu Minh Nguyen <99923860+hieuminh65@users.noreply.github.com>
2023-10-01 16:38:01 +0000

    fix menu and done

--- components/menu.py
+++ components/menu.py
@@ -1,18 +1,21 @@
-import components.login as login
 from components.config import Config
+import components.login as login
 from components.home_screen import home_screen
+import components.menu_helper as menu_helper
+from components.menu_helper import guest_controls_selection, language_option
 import pandas as pd
 
 accounts = pd.read_csv('components/accounts.csv')
 accounts_controls = pd.read_csv('components/accounts_controls.csv')
 
-#function to print 2 groups of links
+# --------------------------Display Functions--------------------------
+"""Function to display 2 groups of links"""
 def display_groups_of_links():
     print("\n1. Useful Links")
-    print("2. InCollege Important Links:")
+    print("2. InCollege Important Links")
     print("3. Exit menu")
 
-#function to print the first set of useful links
+"""Function to display useful links"""
 def display_useful_links():
     print("\nUseful Links:")
     print("1. General")
@@ -21,7 +24,7 @@ def display_useful_links():
     print("4. Directories")
     print("5. Go Back to Previous Menu")
 
-#function to print the inCollege important links
+"""Function to display important links"""
 def display_inCollege_important_link():
     print("\nInCollege Important Links:")
     print("1. A Copyright Notice")
@@ -35,7 +38,7 @@ def display_inCollege_important_link():
     print("9. Languages")
     print("10. Go Back to Previous Menu")
 
-#function to print the links within "General"
+"""Function to display general links in useful links"""
 def display_general_links(isLogin = False):
     print("\nGeneral Links:")
     if not isLogin:
@@ -50,30 +53,9 @@ def display_general_links(isLogin = False):
     print("7. Developers")
     print("8. Go Back to Previous Menu")
 
-#helper function to help handle the links within "General"
-def general_link_selection(choice, isLogin = False):
-    if choice == 1 and not isLogin:
-        print("Redirecting to Sign Up / Login page")
-        print()
-        login.login()
-    elif choice == 1 and isLogin:
-        print("Redirecting to Sign Up page")
-    elif choice == 2:
-        print("\nWe're here to help")
-    elif choice == 3:
-        print("\nIn College: Welcome to In College, the world's largest college student network with many users in many countries and territories worldwide")
-    elif choice == 4:
-        print("\nIn College Pressroom: Stay on top of the latest news, updates, and reports")
-    elif choice == 5:
-        print("\nUnder construction")
-    elif choice == 6:
-        print("\nUnder construction")
-    elif choice == 7:
-        print("\nUnder construction")
-    else:
-        print("\nInvalid Choice. Please Try Again")
 
-#function to handle user's selection for useful links
+# --------------------------Selection Functions--------------------------
+"""Selection function for useful links"""
 def useful_links_user_selection():
     while True:
         display_useful_links()
@@ -97,86 +79,7 @@ def useful_links_user_selection():
         else:
             print("\nInvalid choice. Please try again.") 
 
-
-#function to handle language option
-def language_option():
-    global language_setting
-    
-    while True:
-        #if user is not logged in
-        if Config.FLAG == False:
-            print("Language controls can only be modified by signed-in users.")
-            break
-        else: 
-            print("1. English")
-            print("2. Spanish")
-            choice = int(input("Select an option to change the language:\n"))
-            if choice == 1:
-                print("English was chosen")
-
-                language_setting = "English"
-                
-                break
-
-            elif choice == 2:
-                print("Spanish was chosen")
-
-                language_setting = "Spanish"
-            
-                break
-            else:
-                print("Invalid choice! Please select again.")
-    
-    
-            
-
-#function to handle guest controls
-def guest_controls_selection():
-
-    global sms 
-    global email
-    global advertising
-
-    while True:
-        if Config.FLAG == False:
-            print("Language controls can only be modified by signed-in users.")
-            break
-        else: 
-            print("1. InCollege email")
-            print("2. SMS")
-            print("3. Targeted Advertising features")
-            print("4. Exit")
-            choice = int(input("Select an option to turn on/off one of the above:"))
-            if choice == 1:
-                print("InCollege email: turn on/off. Press 1 for on or 2 for off.")
-                choice = int(input())
-                if choice == 1:
-                    email = "on"
-                elif choice == 2:
-                    email = "off"
-            elif choice == 2:
-                print("InCollege SMS: turn on/off. Press 1 for on or 2 for off.")
-                choice = int(input())
-                if choice == 1:
-                    email = "on"
-                elif choice == 2:
-                    email = "off"
-            elif choice == 3:
-                print("InCollege Advertising: turn on/off. Press 1 for on or 2 for off.")
-                choice = int(input())
-                if choice == 1:
-                    email = "on"
-                elif choice == 2:
-                    email = "off"
-            elif choice == 4:
-                break
-            else:
-                print("Invalid choice! Please select again.")
-    
-  
-
-
-#function to handle user's selection for useful links
+"""Selection function for important links"""
 def incollege_important_links_user_selection():
     while True:
         display_inCollege_important_link()
@@ -191,10 +94,13 @@ def incollege_important_links_user_selection():
             print("By using this website, you agree that your data will be collected for recruiting purposes")
         elif choice == 5:
             print("Privacy Policy\nThis privacy notice for James Anderson LLC describes how and why we might collect, store, use, and/or share your information when you use our services")
+            if not Config.FLAG:
+                print("\nPlease log in to your account before setting")
+                login.login()
             print("Select 1 for Guest Controls, anything else to exit")
-            c = int(input())
+            c = int(input("Your choice: "))
             if c == 1:
-                guest_controls_selection()
+                menu_helper.guest_controls_selection()
         elif choice == 6:
             print("We use cookies for various purposes, including but not limited to:\n\tEssential Cookies: These cookies are necessary for the proper functioning of our website. They enable you to navigate our site and use its features, such as accessing secure areas and making transactions.\n\tPerformance Cookies: These cookies help us analyze how visitors use our website. They allow us to improve the performance and functionality of our site by collecting and reporting information on things like page load times and error messages.\n\tFunctional Cookies: Functional cookies allow us to remember your preferences and settings, such as language preferences and customization options, to enhance your experience on our website.\n\tAdvertising Cookies: We and our advertising partners may use cookies to deliver advertisements that are relevant to your interests. These cookies may collect information about your browsing habits and serve you with tailored advertising content.\n\tAnalytics Cookies: We may use analytics cookies to gather information about how our website is used and to help us improve it. These cookies collect information in an anonymous form, such as the number of visitors to our site and which pages are most ")
         elif choice == 7:
@@ -202,11 +108,38 @@ def incollege_important_links_user_selection():
         elif choice == 8:
             print("4. Restrictions on Use\n\tReproduction and Distribution: Unauthorized reproduction, distribution, or public display of copyrighted materials from our website or platforms is prohibited without our explicit permission or unless permitted by law.\n\tModification and Derivative Works: Users are not allowed to modify, create derivative works from, or alter our copyrighted materials without prior written consent.\n\tRemoval of Copyright Notices: Removing or altering copyright notices, watermarks, or any form of attribution on copyrighted materials is strictly prohibited.")
         elif choice == 9:
-            language_option()
+            if not Config.FLAG:
+                print("\nPlease log in to your account before setting languages")
+                login.login()
+            menu_helper.language_option()
         elif choice == 10:
             break
         else:
             print("\nInvalid choice. Please try again.")
+
+"""Selection function for general links in useful links"""
+def general_link_selection(choice, isLogin = False):
+    if choice == 1 and not isLogin:
+        print("Redirecting to Sign Up / Login page")
+        print()
+        login.login()
+    elif choice == 1 and isLogin:
+        print("Redirecting to Sign Up page")
+    elif choice == 2:
+        print("\nWe're here to help")
+    elif choice == 3:
+        print("\nIn College: Welcome to In College, the world's largest college student network with many users in many countries and territories worldwide")
+    elif choice == 4:
+        print("\nIn College Pressroom: Stay on top of the latest news, updates, and reports")
+    elif choice == 5:
+        print("\nUnder construction")
+    elif choice == 6:
+        print("\nUnder construction")
+    elif choice == 7:
+        print("\nUnder construction")
+    else:
+        print("\nInvalid Choice. Please Try Again")
+
 
 
 def adding_userInformation():
@@ -238,7 +171,7 @@ def adding_userInformation():
 
 
 
-#main function to call both links
+# --------------------------Main Function--------------------------
 def general_menu():
     while True:
         display_groups_of_links()
