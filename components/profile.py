7ece934d0f9e5ca346bb04d79fd006e50c3bf1a5
NPatel-21 <142070922+NPatel-21@users.noreply.github.com>
2023-10-22 00:19:11 -0400

    Made changes into create_profile function so that a user can update its profile whenever it wants.

--- components/profile.py
+++ components/profile.py
@@ -3,57 +3,79 @@ from components.config import Config
 from components import login
 import string
 
-def create_profile():
+def create_profile(username):
     with open('components/profile.json', 'r') as f:
         profiles = json.load(f)
 
-    title = input('Enter a title for your profile: ')
-    major = string.capwords(input('Enter your major: '))
-    university = string.capwords(input('Enter the University you attend currently: '))
-    about = input('Describe yourself: ')
+    if username not in profiles:
+        profiles[username] = {}  # Create an empty profile if it doesn't exist
 
-    #experience section
-    num_of_experience = int(input('How much previous work experience do you have? (Up to 3 previous jobs) '))
-    while num_of_experience > 3 or num_of_experience < 0: #input validation
-        num_of_experience = int(input('Number of previous jobs cannot be greater than 3. Please try again: '))
+    user_profile = profiles[username]
 
-    experience = []
-    for i in range(num_of_experience):
-        new_experience = {}
-        new_experience['title'] = input('Title of Experience: ')
-        new_experience['employer'] = string.capwords(input('Employer: '))
-        new_experience['date_start'] = input('Date Started: ')
-        new_experience['date_end'] = input('Date Ended: ')
-        new_experience['location'] = string.capwords(input('Location: '))
-        new_experience['description'] = input('Description of work done: ')
-        experience.append(new_experience)
+    # Function to update a section of the profile
+    def update_section(section_name, section_data):
+        user_profile[section_name] = section_data
+        print(f"{section_name.capitalize()} section updated successfully.")
 
-    #education section
-    num_of_education = int(input('How many higher level institutions have you attended? '))
-    while num_of_education < 1: #input validation
-        num_of_education = int(input('Number must be 1 or greater. Please try again: '))
+    # Ask the user which section they want to update
+    while True:
+        print("\nProfile Sections:")
+        print("1. Title")
+        print("2. Major")
+        print("3. University")
+        print("4. About")
+        print("5. Experiences")
+        print("6. Education")
+        print("7. Finish and Save")
+        choice = int(input("Select a section to update or 7 to finish: "))
 
-    education = []
-    for i in range(num_of_education):
-        new_education = {}
-        new_education['school'] = string.capwords(input('School: '))
-        new_education['degree'] = string.capwords(input('Degree: '))
-        new_education['year_attended'] = input('Years Attended: ')
-        education.append(new_education)
-
-    username = Config.SYSTEM_ACCOUNT[0]
-
-    profiles[username] = {}
-    profiles[username]['title'] = title
-    profiles[username]['major'] = major
-    profiles[username]['university'] = university
-    profiles[username]['about'] = about
-    profiles[username]['experience'] = experience
-    profiles[username]['education'] = education
-
-
-    with open('components/profile.json', 'w') as f:
-        json.dump(profiles, f, indent=4)
+        if choice == 1:
+            title = input('Enter a title for your profile: ')
+            update_section('title', title)
+        elif choice == 2:
+            major = string.capwords(input('Enter your major: '))
+            update_section('major', major)
+        elif choice == 3:
+            university = string.capwords(input('Enter the University you attend currently: '))
+            update_section('university', university)
+        elif choice == 4:
+            about = input('Describe yourself: ')
+            update_section('about', about)
+        elif choice == 5:
+            num_of_experience = int(input('How much previous work experience do you have? (Up to 3 previous jobs) '))
+            while num_of_experience > 3 or num_of_experience < 0:
+                num_of_experience = int(input('Number of previous jobs cannot be greater than 3. Please try again: '))
+            experience = []
+            for i in range(num_of_experience):
+                new_experience = {}
+                new_experience['title'] = input('Title of Experience: ')
+                new_experience['employer'] = string.capwords(input('Employer: '))
+                new_experience['date_start'] = input('Date Started: ')
+                new_experience['date_end'] = input('Date Ended: ')
+                new_experience['location'] = string.capwords(input('Location: '))
+                new_experience['description'] = input('Description of work done: ')
+                experience.append(new_experience)
+            update_section('experience', experience)
+        elif choice == 6:
+            num_of_education = int(input('How many higher level institutions have you attended? '))
+            while num_of_education < 1:
+                num_of_education = int(input('Number must be 1 or greater. Please try again: '))
+            education = []
+            for i in range(num_of_education):
+                new_education = {}
+                new_education['school'] = string.capwords(input('School: '))
+                new_education['degree'] = string.capwords(input('Degree: '))
+                new_education['year_attended'] = input('Years Attended: ')
+                education.append(new_education)
+            update_section('education', education)
+        elif choice == 7:
+            # Save the updated profile to the JSON file
+            with open('components/profile.json', 'w') as f:
+                json.dump(profiles, f, indent=4)
+            print("Profile saved successfully.")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 7.")
 
 def view_profile(username):
     with open('components/profile.json', 'r') as f:
@@ -62,7 +84,7 @@ def view_profile(username):
     if username in profiles:
         user_profile = profiles[username]
 
-        print("\nUser Profile:")
+        print(f"\n{username}'s profile: ")
         print(f"Title: {user_profile['title']}")
         print(f"Major: {user_profile['major']}")
         print(f"University: {user_profile['university']}")
@@ -119,11 +141,13 @@ def profile():
 
     choice = None
     while choice != 4:
-        print("\n1. View your profile\n2. View a friend's profile\n3. Create a profile\n4. Quit")
+        print("\n1. View your profile\n2. Create/Update your profile\n3. View a friend's profile\n4. Quit")
         choice = int(input("Please enter your choice: "))
         if choice == 1:
             view_profile(Config.SYSTEM_ACCOUNT[0])
         elif choice == 2:
+            create_profile(Config.SYSTEM_ACCOUNT[0])
+        elif choice == 3:
             with open('components/friendLists.json', 'r') as f:
                 friendlists = json.load(f)
             print("Your friends are: ")
@@ -135,10 +159,7 @@ def profile():
             friend_choice = int(input("Enter the username of the friend whose profile you want to view: "))
             friend_username = friendlists[user_id][Config.SYSTEM_ACCOUNT[2]]['friendList'][friend_choice]
             view_friend_profile(friend_username)
-        elif choice == 3:
-            create_profile()
         elif choice == 4:
             break
         else:
             print("Invalid choice. Please enter a number between 1 and 4.")
-
