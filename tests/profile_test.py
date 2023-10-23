c473d84635045b6acece5729c572c8567bdf9e28
luisan08 <133046091+luisan08@users.noreply.github.com>
2023-10-22 20:56:26 -0400

    tests updated

--- tests/profile_test.py
+++ tests/profile_test.py
@@ -4,93 +4,8 @@ from unittest.mock import mock_open, patch
 from components.profile import create_profile, view_profile, view_friend_profile, profile
 
 
-#-------------------------------- view profile test --------------------------------
-"""
-def test_view_profile_existing_user(capsys):
-    # Mock the 'open' function and the file content
-    mock_data = {
-        "user_profile": {
-            "title": "3rd year Computer Science Student",
-            "major": "Computer Science",
-            "university": "University Of South Florida",
-            "about": "Aspiring developer.",
-            "experience": [
-                {
-                    "title": "web developer",
-                    "employer": "Apple",
-                    "date_start": "10/10/24",
-                    "date_end": "10/20/26",
-                    "location": "Florida",
-                    "description": "Develop a website."
-                },
-                {
-                    "title": "Software engineer",
-                    "employer": "Microsoft",
-                    "date_start": "1/12/2021",
-                    "date_end": "21/12/2023",
-                    "location": "New York",
-                    "description": "Bug fixes."
-                }
-            ],
-            "education": [
-                {
-                    "school": "University Of South Florida",
-                    "degree": "Bachelor In Computer Science",
-                    "year_attended": "2020"
-                }
-            ]
-        }
-    }
-    mock_file = json.dumps(mock_data)
-    with patch("builtins.open", new_callable=mock_open, read_data=mock_file):
-        view_profile("user_profile")
-        captured = capsys.readouterr()
-
-    expected_output = \nuser_profile's Profile:
-Title: 3rd Year Computer Science Student
-Major: Computer Science
-University: University Of South Florida
-About: Aspiring developer
-
-Experiences:
-  Title: web developer
-  Employer: Apple
-  Date Started: 10/10/24
-  Date Ended: 10/20/26
-  Location: Florida
-  Description: Develop a website
-
-  Title: Software engineer
-  Employer: Microsoft
-  Date Started: 1/12/2021
-  Date Ended: 21/12/2023
-  Location: New York
-  Description: Bug fixes
-
-Education:
-  School: University Of South Florida
-  Degree: Bachelor In Computer Science
-  Year Attended: 2020
-
-
-    assert captured.out.strip() == expected_output.strip()
-
-"""
-def test_view_profile_non_existing_user(capsys):
-    # Mock the 'open' function and the file content
-    mock_data = {}
-    mock_file = json.dumps(mock_data)
-    with patch("builtins.open", new_callable=mock_open, read_data=mock_file):
-        view_profile("non_existing_user")
-        captured = capsys.readouterr()
-        
-    expected_output = "You don't have a profile. Create one using 'create_profile'.\n"
-    assert captured.out == expected_output
-
-
 
 #---------------------------view friend profile test ---------------------------------------
-
 
 def test_view_friend_profile_existing_user(capsys):
     # Mock the 'open' function and the file content
@@ -215,7 +130,21 @@ def test_create_profile_experience(capsys):
     assert "Experience section updated successfully." in captured.out
 
 
-#---------------------- view profile test -------------------------------
+
+#-------------------------------- view profile test --------------------------------
+
+
+def test_view_profile_non_existing_user(capsys):
+    # Mock the 'open' function and the file content
+    mock_data = {}
+    mock_file = json.dumps(mock_data)
+    with patch("builtins.open", new_callable=mock_open, read_data=mock_file):
+        view_profile("non_existing_user")
+        captured = capsys.readouterr()
+        
+    expected_output = "You don't have a profile. Create one using 'create_profile'.\n"
+    assert captured.out == expected_output
+
 
 
 
